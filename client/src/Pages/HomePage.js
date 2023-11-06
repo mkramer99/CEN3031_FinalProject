49c19cf565a846647b6ecabf76a96eb30c96c732
mkramer99 <92834107+mkramer99@users.noreply.github.com>
2023-11-06 10:05:24 -0500

    Food Fighters project

---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+
+
+function HomePage() {
+    return (
+      <div>
+        <header>Food Fighters</header>
+        <button title="Login"/>
+        <button title="Register a User"/>
+        <button title="Register a Business"/>
+      </div>
+    );
+  }
+  export default HomePage;
+  
