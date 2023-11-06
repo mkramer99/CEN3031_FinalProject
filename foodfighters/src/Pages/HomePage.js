5b5a4a0bad5231aebbb20ed78504305aa368d546
mkramer99 <92834107+mkramer99@users.noreply.github.com>
2023-11-06 18:47:57 -0500

    home page with disfunctinoal buttons

---
+++ foodfighters/src/Pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Button} from 'react';
+function HomePage() {
+    return (
+        <div>
+            <header className="App-header">
+                Food Fighters
+            <button>Login</button>
+            <button>Register A Business</button>
+            <button>Register A User</button>
+            </header>
+        </div>
+    );
+}
+export default HomePage;
