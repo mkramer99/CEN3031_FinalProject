0aebd418797adb1e1a7667e0bb2361760947b969
Melanie Kramer <92834107+mkramer99@users.noreply.github.com>
2023-12-02 09:51:37 -0500

    Update record.js
    Removed commented out code

--- server/routes/record.js
+++ server/routes/record.js
@@ -120,35 +120,4 @@ recordRoutes.route("/Users/Delete/One").post(function (req, res) {
    });
  });
  
-// This section will help you update a record by id.
-// recordRoutes.route("/update/:id").post(function (req, response) {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
-//  let newvalues = {
-//    $set: {
-//      name: req.body.name,
-//      position: req.body.position,
-//      level: req.body.level,
-//    },
-//  };
-//  db_connect
-//    .collection("records")
-//    .updateOne(myquery, newvalues, function (err, res) {
-//      if (err) throw err;
-//      console.log("1 document updated");
-//      response.json(res);
-//    });
-// });
- 
-// // This section will help you delete a record
-// recordRoutes.route("/:id").delete((req, response) => {
-//  let db_connect = dbo.getDb();
-//  let myquery = { _id: ObjectId(req.params.id) };
-//  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
-//    if (err) throw err;
-//    console.log("1 document deleted");
-//    response.json(obj);
-//  });
-// });
- 
 module.exports = recordRoutes;
